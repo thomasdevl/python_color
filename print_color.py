e9933bee61f1bcea5134b7b39ce5fa402c1e885a
thomasdevl <91684310+thomasdevl@users.noreply.github.com>
2022-06-10 22:41:03 +0200

    Update print_color.py

--- print_color.py
+++ print_color.py
@@ -1,7 +1,33 @@
 '''
 Thomas Devlamminck
-Print in color in the Terminal
+Print in color and using Markdown in the Terminal
 '''
+from rich.console import Console
+from rich.markdown import Markdown
+from rich.padding import Padding
+from rich.panel import Panel
+from rich.text import Text
+
+console = Console()
+
+
+def print_markdown(text):
+    """Prints a rich info message. Support Markdown syntax."""
+
+    md = Padding(Markdown(text), 2)
+    console.print(md)
+
+
+def print_step(text):
+    """Prints a rich info message."""
+
+    panel = Panel(Text(text, justify="left"))
+    console.print(panel)
+
+
+def print_substep(text, style=""):
+    """Prints a rich info message without the panelling."""
+    console.print(text, style=style)
 
 class bcolors:
     ResetAll = "\033[0m"
